6aff3ae7c50eeaf0c0dab17aceaa4a501c855b1d
kaelzhang81 <zhangy1981@qq.com>
2017-12-28 15:11:23 +0800

    Update word.go

--- tdd/word/word.go
+++ tdd/word/word.go
@@ -1,7 +1,14 @@
 package word
 
+import "unicode"
+
 func IsPalindrome(word string) bool {
-	letters := []rune(word)
+	var letters []rune
+	for _, s := range word{
+		if unicode.IsLetter(s){
+			letters = append(letters, unicode.ToLower(s))
+		}
+	}
 	for i := range letters {
 		if letters[i] != letters[len(letters)-i-1] {
 			return false
