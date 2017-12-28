24a8c25a1a32fe0256d82b054061d2a2990f2332
kaelzhang81 <zhangy1981@qq.com>
2017-12-28 15:03:43 +0800

    Create word_test.go

---
+++ tdd/word/word_test.go
@@ -0,0 +1,26 @@
+package word
+
+import (
+	"testing"
+)
+
+func TestPalindrome(t *testing.T){
+	if !IsPalindrome("detartrated") {
+		t.Error(`IsPalindrome("detartrated") = false`)
+	}
+	if !IsPalindrome("kayak"){
+		t.Error(`IsPalindrome("kayak") = false`)
+	}
+}
+
+func TestNonPalindrome(t *testing.T) {
+	if IsPalindrome("palindrome"){
+		t.Error(`IsPalindrome("palindrome") = true`)
+	}
+}
+
+func TestFrenchPalindrome(t *testing.T) {
+	if !IsPalindrome("été"){
+		t.Error(`IsPalindrome("été") = false`)
+	}
+}
